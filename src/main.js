a654db46e3af8551e9b6ce743c75396966aa1ebc
apple <apple@appleui-MacBookPro-18.local>
2025-12-30 15:55:44 +0900

    feat: 기본 상단 영역 UI 구현

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import { createHeader } from './components/header';
 import './styles/index.css';
 
 const app = document.querySelector('#app');
 
 app.innerHTML =  `
-  <article class="layout-main"></article>
+  <article class="news"></article>
 `
-const layoutMain = app.querySelector('.layout-main');
+const layoutMain = app.querySelector('.news');
+const header = createHeader();
 
+layoutMain.prepend(header);
