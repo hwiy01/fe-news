54541d5bf0533160b67547200e83485552021b83
apple <apple@appleui-MacBookPro-18.local>
2025-12-30 18:51:59 +0900

    feat: 최신 뉴스 무한 자동 롤링 기능 구현

--- src/components/autoRollingNews/autoRollingNews.js
+++ src/components/autoRollingNews/autoRollingNews.js
@@ -1,14 +1,20 @@
 import { createAutoRollingNewsBar } from "./components/autoRollingNewsBar";
 import './autoRollingNews.css';
+import { autoRollingNewsData1, autoRollingNewsData2 } from "../../data/autoRollingNewsData";
 
 export function createAutoRollingNews() {
     const autoRollingNews = document.createElement('aside');
     autoRollingNews.className = 'autoRollingNews';
-    const autoRollingNewsBarA = createAutoRollingNewsBar();
-    const autoRollingNewsBarB = createAutoRollingNewsBar();
+    const autoRollingNewsBarA = createAutoRollingNewsBar('rolling-news-a', autoRollingNewsData1, 5000);
+    const autoRollingNewsBarB = createAutoRollingNewsBar('rolling-news-b', autoRollingNewsData2, 5000);
     
-    autoRollingNews.prepend(autoRollingNewsBarA);
-    autoRollingNews.prepend(autoRollingNewsBarB);
+    autoRollingNews.prepend(autoRollingNewsBarA.el);
+    autoRollingNews.prepend(autoRollingNewsBarB.el);
+
+    autoRollingNewsBarA.start();
+    setTimeout(() => {
+        autoRollingNewsBarB.start();
+    }, 1000);
 
     return autoRollingNews;
 }
--- src/components/autoRollingNews/components/autoRollingNewsBar.js
+++ src/components/autoRollingNews/components/autoRollingNewsBar.js
@@ -3,16 +3,41 @@ import './autoRollingNewsBar.css';
 export function createAutoRollingNewsBar (classId, newsList, interval) {
     const container = document.createElement('div');
     container.className = 'auto-rolling-news-bar';
-    //container.id = classId;
+    container.id = classId;
 
     container.innerHTML = `
-        <h3>
-            연합뉴스
-        </h3>
-        <div>
-            [속보] 도심 공원 조용한 시범 운영
-        </div>
+        <h3 id="auto-rolling-news-bar__press_${classId}"></h3>
+        <div id="auto-rolling-news-bar__title_${classId}"></div>
     `;
 
-    return container;
+    const pressEl = container.querySelector(`#auto-rolling-news-bar__press_${classId}`);
+    const titleEl = container.querySelector(`#auto-rolling-news-bar__title_${classId}`);
+
+    let currentIndex = 0;
+
+    function render (index) {
+        const { press, mainTitle } = newsList[index];
+
+        pressEl.textContent = press;
+        titleEl.textContent = mainTitle;
+    };
+
+    let intervalId = null;
+
+    function start () {
+        render(currentIndex);
+
+        intervalId = setInterval(() => {
+            currentIndex = (currentIndex + 1) % newsList.length;
+            render(currentIndex);
+        }, interval);
+    }
+
+    function stop () {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+
+
+    return { el: container, start, stop };
 }
